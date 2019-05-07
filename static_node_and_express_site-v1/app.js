9cd37255735ec7a7d685e81c512de763afe2fa22
Austin Scott <austin.scott18@gmail.com>
2019-05-07 14:32:38 -0400

    Revert "move items into docs folder"
    This reverts commit 5e1aeefce344501fa7a16523c279c657a08e6697.

--- static_node_and_express_site-v1/app.js
+++ static_node_and_express_site-v1/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { projects } = require('./data/data.json.js');
+const { projects } = require('./data/data.json');
 const path = require('path');
 const bodyParser = require('body-parser');
 
